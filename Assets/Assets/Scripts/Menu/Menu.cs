64904f9ca1748c65c18b1ebc37dfb95a615f0376
Raphael Mortier <raphaelmortier@outlook.fr>
2022-09-05 12:05:05 +0200

    feat(loading screen)

--- Assets/Assets/Scripts/Menu/Menu.cs
+++ Assets/Assets/Scripts/Menu/Menu.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Cinemachine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
 
+    [Header("Camera")]
     [SerializeField] private CinemachineVirtualCamera CineCamOptions;
     [SerializeField] private CinemachineVirtualCamera CineCamMain;
     [SerializeField] private CinemachineVirtualCamera CineCamOptionsReverse;
+
+    [Header("Menu")]
     [SerializeField] private GameObject MainMenu;
     [SerializeField] private GameObject OptionsMenu;
+
+    [Header("MenuButton")]
+    [SerializeField] private GameObject StartBtn;
+    [SerializeField] private GameObject OptionBtn;
+    [SerializeField] private GameObject ExitBtn;
+
+    [Header("LoadingScreen")]
+    [SerializeField] private GameObject Loader;
+    [SerializeField] private Slider LoaderBar;
+
+    [Header("Scene")]
+    [SerializeField] private Scene GameScene;
 
     void Start() {
         Time.timeScale = 1;
@@ -33,8 +51,26 @@ public class Menu : MonoBehaviour
         CineCamOptions.GetCinemachineComponent<CinemachineTrackedDolly>().m_AutoDolly.m_Enabled = true;
     }
 
-    public void OnStart() {
-        SceneManager.LoadScene("MainStation");
+    public void StartGame() {
+        StartCoroutine(LoadSceneAsync("MainStation"));
+    }
+
+    IEnumerator LoadSceneAsync(string scene) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene,LoadSceneMode.Single);
+
+        Loader.SetActive(true);
+
+        StartBtn.SetActive(false);
+        OptionBtn.SetActive(false);
+        ExitBtn.SetActive(false);
+
+        Loader.SetActive(true);
+
+        while (!operation.isDone) {
+            float progress = Mathf.Clamp01(operation.progress / 0.9F);
+            LoaderBar.value = progress;
+            yield return null;
+        }
     }
 
     public void BackOnMenu() {
