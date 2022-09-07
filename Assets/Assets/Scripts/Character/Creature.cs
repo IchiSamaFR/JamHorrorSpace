0e5395f34fe3b3cfd544c854d7d74e532e71dbd6
Raphael Mortier <raphaelmortier@outlook.fr>
2022-09-07 23:26:07 +0200

    feat jojo le plus beau

--- Assets/Assets/Scripts/Character/Creature.cs
+++ Assets/Assets/Scripts/Character/Creature.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private Waypoint[] IA;
+    private Waypoint lastWaypoint = null;
+    [SerializeField] private Waypoint atm;
+    [SerializeField] private Animator animator;
+
     void Update()
     {
-        
+        Movement();
+    }
+
+    void GoTo(Waypoint newPoint) {
+        lastWaypoint = newPoint;
+        atm = newPoint;
+        animator.SetBool("IsWalking",true);
+        Movement();
+    }
+
+    void Movement() {
+        transform.position = transform.position - atm.waypoint.transform.position * Time.deltaTime * 0.05f;
+        if(transform.position.x <= atm.waypoint.transform.position.x + 0.1f && transform.position.x >= atm.waypoint.transform.position.x -0.1f &&
+            transform.position.y <= atm.waypoint.transform.position.y + 0.1f && transform.position.y >= atm.waypoint.transform.position.y - 0.1f) {
+            ChangeWaypoint();
+        }
+    }
+
+    void ChangeWaypoint() {
+        for (int i = 0; i < atm.waypoints.Length; i++) {
+            // Si on a une seul possiblité alors on y vas direct et on met pas de last waypoint
+            if (atm.waypoints.Length == 1) {
+                lastWaypoint = null;
+                GoTo(atm.waypoints[0]);
+            }
+            int e = Random.Range(0, atm.waypoints.Length);
+            GoTo(atm.waypoints[e]);
+        }
     }
 }
+
+
---
+++ Assets/Assets/Scripts/Character/Waypoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    [SerializeField] public GameObject waypoint;
+    [SerializeField] public Waypoint[] waypoints;
+
+}
