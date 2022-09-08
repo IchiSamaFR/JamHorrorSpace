b1d347538f4d2378c042bcddbabdfdab0671d456
IchiSamaFR <djordan-dsousa@hotmail.fr>
2022-09-08 22:59:29 +0200

    Debug vent and casier

--- Assets/Assets/Scripts/Character/Creature.cs
+++ Assets/Assets/Scripts/Character/Creature.cs
@@ -2,39 +2,60 @@ using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
-
+    [SerializeField] private CharacterStats characterStats;
     [SerializeField] private Waypoint[] IA;
     private Waypoint lastWaypoint = null;
     [SerializeField] private Waypoint atm;
     [SerializeField] private Animator animator;
 
+    private void Awake()
+    {
+        characterStats = GetComponent<CharacterStats>();
+    }
+
+    private void Start()
+    {
+        transform.position = atm.transform.position;
+    }
     void Update()
     {
         Movement();
     }
 
     void GoTo(Waypoint newPoint) {
+        print(atm.name);
+        print(newPoint.name);
         lastWaypoint = atm;
         atm = newPoint;
         animator.SetBool("IsWalking",true);
         Movement();
     }
 
-    void Movement() {
-        transform.position = transform.position - atm.waypoint.transform.position * Time.deltaTime * 0.05f;
-        if(transform.position.x <= atm.waypoint.transform.position.x + 0.1f && transform.position.x >= atm.waypoint.transform.position.x -0.1f &&
-            transform.position.y <= atm.waypoint.transform.position.y + 0.1f && transform.position.y >= atm.waypoint.transform.position.y - 0.1f) {
+    void Movement()
+    {
+        var heading = atm.transform.position - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        direction.y = 0;
+        transform.position += direction * Time.deltaTime * characterStats.WalkSpeed;
+        if(transform.position.x <= atm.transform.position.x + 0.1f && transform.position.x >= atm.transform.position.x - 0.1f &&
+            transform.position.z <= atm.transform.position.z + 0.1f && transform.position.z >= atm.transform.position.z - 0.1f) {
             ChangeWaypoint();
         }
     }
 
     void ChangeWaypoint() {
+        print("------");
+        foreach (var item in atm.waypoints)
+        {
+            print(item.name);
+        }
+        if(atm.waypoints.Length == 1)
+        {
+            GoTo(atm.waypoints[0]);
+            return;
+        }
         for (int i = 0; i < atm.waypoints.Length; i++) {
-            // Si on a une seul possiblité alors on y vas direct et on met pas de last waypoint
-            if (atm.waypoints.Length == 1) {
-                lastWaypoint = null;
-                GoTo(atm.waypoints[0]);
-            }
             int e = Random.Range(0, atm.waypoints.Length);
             GoTo(atm.waypoints[e]);
         }
